182caa4a370e6cbd27a931c005bfdbbc02008e89
dRamilowski <67924799+beyus723@users.noreply.github.com>
2023-02-26 01:09:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from getpass import getpass as input
+
+#Selecting options - R P or S
+
+player1 = input("Player 1: Select: R or P or S \n")
+
+player2 = input("\nPlayer 2: Select: R or P or S \n")
+
+#Choosing process
+
+if (player1 == "r" or player1 == "R") and (player2 == "p" or player2 == "P"):
+  if (player1 == "r" or player1 == "R"):
+  
+  
+    print("\nPlayer 2 is a winner!")
+  else:
+    print("nara")
+
